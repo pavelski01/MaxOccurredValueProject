6cb60e865b2aed2873547420d5d0b07ad696f2e1
Pavel Ski <pavelski01@gmail.com>
2015-02-06 18:21:45 +0100

    Update OccurredValue.java

--- src/OccurredValue.java
+++ src/OccurredValue.java
@@ -3,13 +3,38 @@ import java.util.Random;
 public class OccurredValue
 {
 	static final int ARR_SI = 10;
-	static Random random = new Random();
+	static final Random random = new Random();
 	
 	static
 	{ random.setSeed(System.currentTimeMillis()); }
 	
 	static int getRandom(int _low, int _high)
 	{ return random.nextInt(_high - _low + 1) + _low; }
+	
+	static int solution(int _max, int[] _array)
+	{
+		int ambit = _array.length;
+		int[] count = new int[ambit];
+		for (int i = 0; i < ambit; i++) count[i] = 0;
+		int maxOccurence = _max;
+		int index = -1;
+		for (int i = 0; i < ambit; i++)
+		{
+			if (count[_array[i]] > 0)
+			{
+				int tmp = count[_array[i]];
+				count[_array[i]] = ++tmp;
+				if (tmp >= maxOccurence)
+				{
+					maxOccurence = tmp;
+					index = i;
+					
+				}
+			}
+			else count[_array[i]] = 1;
+		}
+		return (index > -1) ? _array[index] : index;
+	}
 	
 	public static void main(final String[] _args)
 	{
@@ -26,28 +51,4 @@ public class OccurredValue
 		System.out.print("Max occurred(value): " + 
 			Integer.toString(solution(minOcc, array)));
 	}
-	
-    static int solution(int _max, int[] _array)
-    {
-        int ambit = _array.length;
-        int[] count = new int[ambit];
-        for (int i = 0; i < ambit; i++) count[i] = 0;
-        int maxOccurence = _max;
-        int index = -1;
-        for (int i = 0; i < ambit; i++)
-        {
-            if (count[_array[i]] > 0)
-            {
-                int tmp = count[_array[i]];
-                count[_array[i]] = ++tmp;
-                if (tmp >= maxOccurence)
-                {
-                    maxOccurence = tmp;
-                    index = i;
-                }                
-            } 
-            else count[_array[i]] = 1;
-        }
-        return (index > -1) ? _array[index] : index;
-    }
 }
